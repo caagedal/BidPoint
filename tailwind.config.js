0d3e76b63bc5ff6f866120d963912956a53e5ff5
caagedal <caagedal@gmail.com>
2024-10-01 00:06:02 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,mjs}","./index.html","./listing/index.html","./profile/index.html" ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
